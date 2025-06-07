ee9617e0b94bf873d0ada1e92a875030ca89bcc5
subhadarshani <subhadarshani82@gmail.com>
2025-06-07 23:17:20 +0530

    updated models.py and admin.py (create CustomUser Model ,register in admin.py and apply migrations)

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . models import CustomUser
 # Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active')  # Columns to show
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+    
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    USER_ROLES = (
+        ('admin', 'Admin'),
+        ('distributor', 'Distributor'),
+        ('seller', 'Seller'),
+    )
+    role = models.CharField(max_length=20, choices=USER_ROLES)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
